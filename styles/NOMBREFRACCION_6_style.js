d505225fce1a62cce64c3a632c36227eeb085c9f
PPIWEB <paraguaypropiedadeswebapp@gmail.com>
2025-11-04 16:21:35 -0300

    dddd

---
+++ styles/NOMBREFRACCION_6_style.js
@@ -0,0 +1,32 @@
+var size = 0;
+var placement = 'point';
+
+var style_NOMBREFRACCION_6 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("");
+    var labelFont = "13.0px \'Open Sans\', sans-serif";
+    var labelFill = "#323232";
+    var bufferColor = "#fafafa";
+    var bufferWidth = 3.0;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'point';
+    if (feature.get("NAME") !== null && resolution > 0 && resolution < 2) {
+        labelText = String(feature.get("NAME"));
+    }
+    var style = [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 0.0 + size,
+            displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.0}), fill: new ol.style.Fill({color: 'rgba(213,180,60,0.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
