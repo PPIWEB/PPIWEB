d4ded99c5c947009da66f24f40c4e0dd032f9aec
PPIWEB <paraguaypropiedadeswebapp@gmail.com>
2025-11-06 09:06:55 -0300

    Actualizacion automatica de datos del mapa

--- layers/COMBINADO_3.js
+++ layers/COMBINADO_3.js
@@ -79771,7 +79771,7 @@ var json_COMBINADO_3 = {
         "Manzana": "5",
         "Lote": "2",
         "Superficie": "360",
-        "Estado": "Libre",
+        "Estado": "Vendido",
         "Cuota": "600.00Gs.",
         "Total": "78.000.000",
         "Descuento": "50%",
