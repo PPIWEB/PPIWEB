b6a8840c46eb448bcb1371d06a07c8c2e3868700
PPIWEB <paraguaypropiedadeswebapp@gmail.com>
2026-02-28 14:10:44 -0300

    Add files via upload

---
+++ layers/NOMBREFRACCION_6.js
@@ -0,0 +1,359 @@
+var json_NOMBREFRACCION_6 = {
+  type: "FeatureCollection",
+  name: "NOMBREFRACCION_6",
+  crs: { type: "name", properties: { name: "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+  features: [
+    {
+      type: "Feature",
+      properties: { NAME: "RESIDENCIAL ÑA GUILLERMINA" },
+      geometry: {
+        type: "Point",
+        coordinates: [-56.392773786626336, -24.394172273093083],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { NAME: "PARQUE DEL NORTE" },
+      geometry: {
+        type: "Point",
+        coordinates: [-56.409778970181094, -24.389713189715994],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { NAME: "LOS MANGALES 1" },
+      geometry: {
+        type: "Point",
+        coordinates: [-57.428172409305468, -25.39413214651502],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { NAME: "LOS MANGALES 2" },
+      geometry: {
+        type: "Point",
+        coordinates: [-57.427898588045231, -25.393249610818224],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { NAME: "EL RODEO" },
+      geometry: {
+        type: "Point",
+        coordinates: [-56.726555777741567, -25.333945358189819],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { NAME: "LA MANSION 1" },
+      geometry: {
+        type: "Point",
+        coordinates: [-57.229084920640375, -25.182124491628667],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { NAME: "ALTOS DEL MONTE 1" },
+      geometry: {
+        type: "Point",
+        coordinates: [-57.414869995824461, -25.445395907644958],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { NAME: "ALTOS DEL MONTE 2" },
+      geometry: {
+        type: "Point",
+        coordinates: [-57.416724914038937, -25.445048148939691],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { NAME: "BRISAS DEL SUR" },
+      geometry: {
+        type: "Point",
+        coordinates: [-56.236378623562217, -27.148335343978143],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { NAME: "COSTA ARENA" },
+      geometry: {
+        type: "Point",
+        coordinates: [-56.873244476730761, -25.621478080709306],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { NAME: "CUATRO BOCAS" },
+      geometry: {
+        type: "Point",
+        coordinates: [-56.551110546661434, -24.179253023882957],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { NAME: "SALADO I" },
+      geometry: {
+        type: "Point",
+        coordinates: [-57.446211047293886, -25.17338253076689],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { NAME: "SALADO II" },
+      geometry: {
+        type: "Point",
+        coordinates: [-57.444773044030477, -25.184781488608607],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { NAME: "NACIENTES DE YTU" },
+      geometry: {
+        type: "Point",
+        coordinates: [-57.117300482681003, -25.428660875623475],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { NAME: "PANAMBI" },
+      geometry: {
+        type: "Point",
+        coordinates: [-57.23344839491623, -25.165701622037933],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { NAME: "ECOBARRIO CERRO VISTA" },
+      geometry: {
+        type: "Point",
+        coordinates: [-57.055741930203311, -25.53008189700989],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { NAME: "EL PROGRESO" },
+      geometry: {
+        type: "Point",
+        coordinates: [-57.232860120853921, -25.151858296587008],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { NAME: "ISLA VERDE" },
+      geometry: {
+        type: "Point",
+        coordinates: [-56.989425954152736, -25.460223372406048],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { NAME: "VILLA VERDE" },
+      geometry: {
+        type: "Point",
+        coordinates: [-57.363823309032867, -25.128672106173632],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { NAME: "NUEVO AMANECER II" },
+      geometry: {
+        type: "Point",
+        coordinates: [-57.486738802044059, -25.49499985027672],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { NAME: "ESPERANZA I" },
+      geometry: {
+        type: "Point",
+        coordinates: [-56.958589926249395, -25.416509770088656],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { NAME: "ESPERANZA II" },
+      geometry: {
+        type: "Point",
+        coordinates: [-56.959466375105734, -25.419056924842756],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { NAME: "LA ARBOLEDA" },
+      geometry: {
+        type: "Point",
+        coordinates: [-57.501310757977834, -25.427817406965818],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { NAME: "AIRES DE VILLA REAL" },
+      geometry: {
+        type: "Point",
+        coordinates: [-57.23769342493064, -25.334052960384906],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { NAME: "BOSQUES DE ITACURUBI I" },
+      geometry: {
+        type: "Point",
+        coordinates: [-56.8057949303507, -25.46179249258433],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { NAME: "BOSQUES DE ITACURUBI II" },
+      geometry: {
+        type: "Point",
+        coordinates: [-56.807750544125362, -25.464833927090083],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { NAME: "ECOBARRIO VISTA VERDE I" },
+      geometry: {
+        type: "Point",
+        coordinates: [-56.930447421773628, -25.364541864513125],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { NAME: "ECOBARRIO BOSQUES DE YACAREY" },
+      geometry: {
+        type: "Point",
+        coordinates: [-57.020766370753392, -25.426075457856577],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { NAME: "RESIDENCIAL LAS MERCEDES" },
+      geometry: {
+        type: "Point",
+        coordinates: [-57.135414228292582, -25.432052365233883],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { NAME: "ECOBARRIO COLINAS DE ITACURUBI" },
+      geometry: {
+        type: "Point",
+        coordinates: [-56.850064540950854, -25.446269213262326],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { NAME: "ECOMIRADOR DEL YHAGUY" },
+      geometry: {
+        type: "Point",
+        coordinates: [-56.841148760620825, -25.449739043130492],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { NAME: "TIERRA DE REYES" },
+      geometry: {
+        type: "Point",
+        coordinates: [-56.964336777551829, -25.443472279943595],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { NAME: "RESIDENCIAL DERLIS JULIAN" },
+      geometry: {
+        type: "Point",
+        coordinates: [-56.852586567251947, -25.46749310063338],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { NAME: "RESIDENCIAL SAN AGUSTIN" },
+      geometry: {
+        type: "Point",
+        coordinates: [-56.929855669740085, -25.521041529070516],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { NAME: "RESIDENCIAL ACCA 01" },
+      geometry: {
+        type: "Point",
+        coordinates: [-57.341305181565183, -25.19839594033537],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { NAME: "RESIDENCIAL VALENTINA" },
+      geometry: {
+        type: "Point",
+        coordinates: [-56.877502428218605, -25.582370212235151],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { NAME: "RESIDENCIAL ALTO PARAISO" },
+      geometry: {
+        type: "Point",
+        coordinates: [-57.479614293539207, -25.205179907172734],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { NAME: "RESIDENCIAL SAGRADO NORTEÑO" },
+      geometry: {
+        type: "Point",
+        coordinates: [-56.512862039223066, -23.794294396907791],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { NAME: "RESIDENCIAL NUEVO AMANECER I" },
+      geometry: {
+        type: "Point",
+        coordinates: [-57.485576048999341, -25.494894040559139],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { NAME: "VILLA ROSITA" },
+      geometry: {
+        type: "Point",
+        coordinates: [-57.418914091539399, -25.313235886276956],
+      },
+    },
+{
+      type: "Feature",
+      properties: { NAME: "TIERRA DE REYES" },
+      geometry: {
+        type: "Point",
+        coordinates: [-56.958659509412115, -25.671816468833221],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { NAME: "46-MARIA JOSE I" },
+      geometry: {
+        type: "Point",
+        coordinates: [-57.064785151308229, -25.359820394176008],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { NAME: "47-RES MARIA Y JOSE II" },
+      geometry: {
+        type: "Point",
+        coordinates: [-57.061239707601807, -25.358729845281061],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { NAME: "48- RES. MARIA Y JOSE III" },
+      geometry: {
+        type: "Point",
+        coordinates: [-57.06492407240377, -25.357926951416029],
+      },
+    },
+  ],
+};
