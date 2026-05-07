48258237c92288f56d74596b95a233815ce2d280
PPIWEB <paraguaypropiedadeswebapp@gmail.com>
2026-05-07 12:03:30 -0300

    Update labelText assignment based on feature 'Lote'

--- styles/COMBINADO_3_style.js
+++ styles/COMBINADO_3_style.js
@@ -93,8 +93,9 @@ var style_COMBINADO_3 = function(feature, resolution){
     var offsetX = 0;
     var offsetY = 0;
     var placement = 'point';
-    if (feature.get("id") !== null && resolution < 10.0) { 
-        labelText = String(feature.get("id"));
+    if (feature.get("Lote") !== null && resolution < 1.5) {
+    labelText = String(feature.get("Lote"));
+}
     }
     
     var style = categories_COMBINADO_3(feature, value, size, resolution, labelText,
